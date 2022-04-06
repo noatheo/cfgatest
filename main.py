ec649f0a46c8fea13e2550ee42735e8d4de665ff
Nawras <99247994+noatheo@users.noreply.github.com>
2022-04-07 01:02:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def hello_gcs(event, context):
-    """Triggered by a change to a Cloud Storage bucket.
+    """Triggered by a change to a Cloud Sftorage bucket.
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
